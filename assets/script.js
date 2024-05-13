a609fcbb158e88a93aa039caae09c2531097d85a
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2024-05-13 14:00:07 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//FUNZIONE PER LO SCORRIMENTO MANUALE DELLO SNAP - SCROLL
 document.querySelectorAll('.carousel-arrow').forEach(function(button) {
     button.addEventListener('click', function() {
       var targetId = this.getAttribute('data-target');
@@ -12,3 +13,29 @@ document.querySelectorAll('.carousel-arrow').forEach(function(button) {
       }
     });
   });
+
+
+//FUNZIONE PER LO SCORRIMENTO AUTOMATICO DELLO SNAP - SCROLL
+  window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    
+    if (scrollPosition > 50 && scrollPosition < 70) {
+        document.querySelector('.carousel-arrow[data-target="scroll-container1"].right').click();
+    } else if (scrollPosition > 500 && scrollPosition < 520) {
+        document.querySelector('.carousel-arrow[data-target="scroll-container2"].right').click();
+    } else if (scrollPosition > 1000 && scrollPosition < 1050) {
+        document.querySelector('.carousel-arrow[data-target="scroll-container3"].right').click();
+    }
+});
+
+
+
+//FUNZIONE PER FAR PARTIRE TUTTI GLI SNAP - SCROLL SEMPRE DA SINISTRA VERSO DESTRA
+window.onload = function() {
+  window.scrollTo(0, 0);
+};
+
+
+
+
+
